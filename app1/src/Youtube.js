47d4b42a319156cf8827278d44ca8846b841b525
Morinaga <s0820hika@gmail.com>
2021-03-13 16:57:44 +0900

    burry Youtube

--- app1/src/Youtube.js
+++ app1/src/Youtube.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import Movie from './movie.json';
+import React from "react";
+import Movie from "./movie.json";
 
-class Youtube extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render(){
-    return (
-      <div style={{margin: '20px', textAlign: 'center'}}>
-                <iframe 
-                  id="ytplayer" 
-                  type="ytplayer" 
-                  width="480" 
-                  height="270"
-                  src={this.props.url}
-                  frameborder="0"
-                />
-            </div>
-    );
-  }
+export default function Youtube() {
+  return (
+    <div style={{ margin: "20px", textAlign: "center" }}>
+      <iframe
+        title="ymovie"
+        id="ytplayer"
+        type="ytplayer"
+        width="480"
+        height="270"
+        src={Movie.movies.movie1.url}
+        frameborder="0"
+      />
+    </div>
+  );
 }
-
-export default Youtube;
