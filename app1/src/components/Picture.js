e7c2808f383ce0d90fa11197d966930d25c62bb5
nohara-maker <65494136+nohara-maker@users.noreply.github.com>
2021-03-14 12:55:59 +0900

    Add files via upload

---
+++ app1/src/components/Picture.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Picture(){
+  return(
+    <div className="picture">
+      <img src="logo192.png"></img>
+      <h1 className="comment">頑張りましょう！</h1>
+    </div>
+  );
+}
+
+export default Picture;
