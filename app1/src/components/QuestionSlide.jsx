8f21c62740d1b3ec569ed6f3301fc86415061bcd
Morinaga <s0820hika@gmail.com>
2021-03-14 13:30:50 +0900

    create culScore

--- app1/src/components/QuestionSlide.jsx
+++ app1/src/components/QuestionSlide.jsx
@@ -3,12 +3,11 @@ import Slider from "react-slick";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import styles from "../assets/QuestionStyle.module.css";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-function QuestionSlide() {
+function QuestionSlide(props) {
   const settings = {
     dots: true,
     infinite: true,
@@ -22,56 +21,72 @@ function QuestionSlide() {
     color: "#fff",
     background: "#3ab60b",
   };
-  const [answer1, setAnswer1] = React.useState(false);
-  const [answer2, setAnswer2] = React.useState(false);
-  const [answer3, setAnswer3] = React.useState(false);
-  const [answer4, setAnswer4] = React.useState(false);
-  const [answer5, setAnswer5] = React.useState(false);
+  // const [count, setCount] = useState(0);
+  // const [answer1, setAnswer1] = React.useState("いいえ");
+  // const [answer2, setAnswer2] = React.useState("いいえ");
+  // const [answer3, setAnswer3] = React.useState("いいえ");
+  // const [answer4, setAnswer4] = React.useState("いいえ");
+  // const [answer5, setAnswer5] = React.useState("いいえ");
 
-  const handleChange1 = (event) => {
-    setAnswer1(event.target.value);
-  };
-  const handleChange2 = (event) => {
-    setAnswer2(event.target.value);
-  };
-  const handleChange3 = (event) => {
-    setAnswer3(event.target.value);
-  };
-  const handleChange4 = (event) => {
-    setAnswer4(event.target.value);
-  };
-  const handleChange5 = (event) => {
-    setAnswer5(event.target.value);
-  };
+  // const handleChange1 = (event) => {
+  //   setAnswer1(event.target.value);
+  //   console.log(answer1);
+  // };
+  // const handleChange2 = (event) => {
+  //   setAnswer2(event.target.value === "はい");
+  // };
+  // const handleChange3 = (event) => {
+  //   if (event.target.value === "はい") setAnswer3(true);
+  //   else setAnswer3(false);
+  // };
+  // const handleChange4 = (event) => {
+  //   if (event.target.value === "はい") setAnswer4(true);
+  //   else setAnswer4(false);
+  // };
+  // const handleChange5 = (event) => {
+  //   if (event.target.value === "はい") setAnswer5(true);
+  //   else setAnswer5(false);
+  // };
+  // const [count, setCount] = useState(0);
+  // function handleChange() {
+  //   if (count <= 5) {
+  //     let p = 0;
+  //     let elements = document.getElementsByName("a");
+  //     for (let i = 0; i < elements.length; i++) {
+  //       if (elements[i].checked) {
+  //         p += 1;
+  //       }
+  //     }
+  //     setCount(p);
+  //   }
+  // }
 
   return (
     <Slider {...settings}>
       <div>
         <h1 style={style}>
-          <p>1</p>
+          {/* <p>1あ</p> */}
+          <p>朝すぐにベットから出れる？</p>
           <RadioGroup
             aria-label="question1"
             name="question1"
-            value={answer1}
-            onChange={handleChange1}
+            value={props.answer1}
+            onChange={props.handleChange1}
           >
-            <FormControlLabel value="はい" control={<Radio />} label="はい" />
-            <FormControlLabel
-              value="いいえ"
-              control={<Radio />}
-              label="いいえ"
-            />
+            <FormControlLabel value="いいえ" control={<Radio />} label="はい" />
+            <FormControlLabel value="はい" control={<Radio />} label="いいえ" />
           </RadioGroup>
         </h1>
       </div>
       <div>
         <h1 style={style}>
           <p>2</p>
+          <p>１日のルーティンはある？</p>
           <RadioGroup
             aria-label="question2"
             name="question2"
-            value={answer2}
-            onChange={handleChange2}
+            value={props.answer2}
+            onChange={props.handleChange2}
           >
             <FormControlLabel value="はい" control={<Radio />} label="はい" />
             <FormControlLabel
@@ -85,11 +100,12 @@ function QuestionSlide() {
       <div>
         <h1 style={style}>
           <p>3</p>
+          <p>運動してる？</p>
           <RadioGroup
             aria-label="question3"
             name="question3"
-            value={answer3}
-            onChange={handleChange3}
+            value={props.answer3}
+            onChange={props.handleChange3}
           >
             <FormControlLabel value="はい" control={<Radio />} label="はい" />
             <FormControlLabel
@@ -103,11 +119,12 @@ function QuestionSlide() {
       <div>
         <h1 style={style}>
           <p>4</p>
+          <p>夢がある？</p>
           <RadioGroup
             aria-label="question4"
             name="question4"
-            value={answer4}
-            onChange={handleChange4}
+            value={props.answer4}
+            onChange={props.handleChange4}
           >
             <FormControlLabel value="はい" control={<Radio />} label="はい" />
             <FormControlLabel
@@ -121,11 +138,12 @@ function QuestionSlide() {
       <div>
         <h1 style={style}>
           <p>5</p>
+          <p>やる気はある？</p>
           <RadioGroup
             aria-label="question5"
             name="question5"
-            value={answer5}
-            onChange={handleChange5}
+            value={props.answer5}
+            onChange={props.handleChange5}
           >
             <FormControlLabel value="はい" control={<Radio />} label="はい" />
             <FormControlLabel
@@ -136,6 +154,7 @@ function QuestionSlide() {
           </RadioGroup>
         </h1>
       </div>
+      {/* {count} */}
     </Slider>
   );
 }
--- app1/src/pages/QuestionPage.jsx
+++ app1/src/pages/QuestionPage.jsx
@@ -2,21 +2,193 @@ import React from "react";
 // import Question from "../components/Question";
 import { Link } from "react-router-dom";
 // import { Redirect } from "react-router-dom";
-import QuestionSlide from "../components/QuestionSlide";
+// import QuestionSlide from "../components/QuestionSlide";
 import { Button } from "@material-ui/core";
 import styles from "../assets/QuestionStyle.module.css";
+import Slider from "react-slick";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import QuestionSlide from "../components/QuestionSlide";
 
 export default function QuestionPage() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const style = {
+    margin: 100,
+    height: 300,
+    color: "#fff",
+    background: "#3ab60b",
+  };
+  const [answer1, setAnswer1] = React.useState("はい");
+  const [answer2, setAnswer2] = React.useState("はい");
+  const [answer3, setAnswer3] = React.useState("はい");
+  const [answer4, setAnswer4] = React.useState("はい");
+  const [answer5, setAnswer5] = React.useState("はい");
+
+  function culSum() {
+    let Score = 0;
+    if (answer1 === "いいえ") Score += 1;
+    if (answer2 === "いいえ") Score += 2;
+    if (answer3 === "いいえ") Score += 3;
+    if (answer4 === "いいえ") Score += 4;
+    if (answer5 === "いいえ") Score += 5;
+    console.log(Score);
+    return Score;
+  }
+
+  const handleChange1 = (event) => {
+    setAnswer1(event.target.value);
+    console.log(answer1);
+  };
+  const handleChange2 = (event) => {
+    setAnswer2(event.target.value);
+  };
+  const handleChange3 = (event) => {
+    setAnswer3(event.target.value);
+  };
+  const handleChange4 = (event) => {
+    setAnswer4(event.target.value);
+  };
+  const handleChange5 = (event) => {
+    setAnswer5(event.target.value);
+  };
   return (
-    <div className={styles.color}>
+    <div className={styles.QuestionSlide}>
       <h1>質問</h1>
-      <QuestionSlide />
+      <Slider {...settings}>
+        <div>
+          <h1 style={style}>
+            <p>1</p>
+            <p>朝すぐにベットから出れる？</p>
+            <RadioGroup
+              aria-label="question1"
+              name="question1"
+              value={answer1}
+              onChange={handleChange1}
+            >
+              <FormControlLabel
+                value="いいえ"
+                control={<Radio />}
+                label="はい"
+              />
+              <FormControlLabel
+                value="はい"
+                control={<Radio />}
+                label="いいえ"
+              />
+            </RadioGroup>
+          </h1>
+        </div>
+        <div>
+          <h1 style={style}>
+            <p>2</p>
+            <p>１日のルーティンはある？</p>
+            <RadioGroup
+              aria-label="question2"
+              name="question2"
+              value={answer2}
+              onChange={handleChange2}
+            >
+              <FormControlLabel
+                value="いいえ"
+                control={<Radio />}
+                label="はい"
+              />
+              <FormControlLabel
+                value="はい"
+                control={<Radio />}
+                label="いいえ"
+              />
+            </RadioGroup>
+          </h1>
+        </div>
+        <div>
+          <h1 style={style}>
+            <p>3</p>
+            <p>運動してる？</p>
+            <RadioGroup
+              aria-label="question3"
+              name="question3"
+              value={answer3}
+              onChange={handleChange3}
+            >
+              <FormControlLabel
+                value="いいえ"
+                control={<Radio />}
+                label="はい"
+              />
+              <FormControlLabel
+                value="はい"
+                control={<Radio />}
+                label="いいえ"
+              />
+            </RadioGroup>
+          </h1>
+        </div>
+        <div>
+          <h1 style={style}>
+            <p>4</p>
+            <p>夢がある？</p>
+            <RadioGroup
+              aria-label="question4"
+              name="question4"
+              value={answer4}
+              onChange={handleChange4}
+            >
+              <FormControlLabel
+                value="いいえ"
+                control={<Radio />}
+                label="はい"
+              />
+              <FormControlLabel
+                value="はい"
+                control={<Radio />}
+                label="いいえ"
+              />
+            </RadioGroup>
+          </h1>
+        </div>
+        <div>
+          <h1 style={style}>
+            <p>5</p>
+            <p>やる気はある？</p>
+            <RadioGroup
+              aria-label="question5"
+              name="question5"
+              value={answer5}
+              onChange={handleChange5}
+            >
+              <FormControlLabel
+                value="いいえ"
+                control={<Radio />}
+                label="はい"
+              />
+              <FormControlLabel
+                value="はい"
+                control={<Radio />}
+                label="いいえ"
+              />
+            </RadioGroup>
+          </h1>
+        </div>
+        {/* {count} */}
+      </Slider>
       <div className={styles.resultbButton}>
         <Button
           variant="contained"
           color="primary"
           component={Link}
           to="/youtube"
+          onClick={culSum}
         >
           動画を見る
         </Button>
